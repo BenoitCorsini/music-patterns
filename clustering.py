0b95d29b59cc80e7ce157e53b504889b1c9ec9db
Benoît Corsini <benoitcorsini@gmail.com>
2020-10-30 16:22:14 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -3,50 +3,63 @@ import os.path as osp
 import numpy as np
 from time import time
 from shutil import copyfile, rmtree
-from random import sample
+from random import sample, random
 from sklearn.cluster import AgglomerativeClustering as AC
 import argparse
 import json
 
 
-class SongClustering(object):
+class ClusteringParams(object):
 
     def __init__(self, cmd):
         self.im_dir = cmd['im_dir']
         self.res_dir = cmd['res_dir']
-        self.clusters_dir = cmd['clusters_dir']
+        if not osp.exists(self.res_dir):
+            os.makedirs(self.res_dir)
 
-        self.AC_division = cmd['AC_division']
-        self.clusters_threshold = cmd['clusters_threshold']
-        self.clusters_ordering = cmd['clusters_ordering']
-        self.write_clusters_info = cmd['write_clusters_info']
+        self.order_by = cmd['order_by']
+        self.n_folders = cmd['n_folders']
+        self.write_info = cmd['write_info']
 
-        self.clusters = {}
-        self.clusters_info = {}
-
-        self.__get_measures__()
-
-    def __get_measures__(self):
-        self.dists = np.loadtxt(osp.join(self.res_dir, 'dists.txt'), delimiter='\t')
         self.song_dict = {}
         for line in open(osp.join(self.res_dir, 'song_list.txt'), 'r'):
             index_song, song = line.split('\n')[0].split('\t')
             index_song = int(index_song)
-            self.song_dict[index_song] = song.split('.txt')[0]
+            self.song_dict[index_song] = song
+
         self.n_songs = len(self.song_dict)
+        self.dists = np.loadtxt(osp.join(self.res_dir, 'dists.txt'), delimiter='\t')
+
+        if self.n_folders is None:
+            self.n_folders = self.n_songs
+
+        self.groups = {}
+
+
+class SongClustering(ClusteringParams):
+
+    def __init__(self, cmd):
+        super().__init__(cmd)
+        self.clusters_dir = cmd['clusters_dir']
+        self.AC_division = cmd['AC_division']
+        self.cluster_size_threshold = cmd['cluster_size_threshold']
+        self.clusters = {}
 
     def get_clusters_recursive(self, labels, cluster_id):
         cluster_dists = self.dists[labels,:][:,labels]
         cluster_size = np.sum(labels)
-        if cluster_size <= self.clusters_threshold:
-            cluster_dists = np.mean(cluster_dists)
-            self.clusters_info[cluster_id] = {
-                  'dist' : cluster_dists,
+        if cluster_size <= self.cluster_size_threshold:
+            cluster_songs = []
+            for index_song in np.where(labels)[0]:
+                cluster_songs.append(self.song_dict[index_song])
+
+            self.clusters[cluster_id] = {
+                  'dist' : np.mean(cluster_dists),
+                  'std_dist' : np.std(cluster_dists),
+                  'songs' : cluster_songs,
                   'size' : int(cluster_size),
                   'depth' : len(cluster_id)
             }
-            for index_song in np.where(labels)[0]:
-                self.clusters[self.song_dict[index_song]] = cluster_id
         else:
             cluster_labels = AC(n_clusters=self.AC_division, affinity='precomputed', linkage='complete').fit_predict(cluster_dists)
             for l in range(self.AC_division):
@@ -57,24 +70,33 @@ class SongClustering(object):
     def get_clusters(self):
         self.get_clusters_recursive(np.ones(self.n_songs) == 1, '')
 
-        for (index_cluster, (cluster_id, cluster_info)) in enumerate(sorted(self.clusters_info.items(), key=lambda x:x[1].get(self.clusters_ordering, x[0]))):
-            if cluster_info['size'] == 1:
-                self.clusters_info[cluster_id]['name'] = 'Singleton Cluster'
+        index_cluster = 1
+        for cluster_info in sorted(self.clusters.values(), key=lambda x:x.get(self.order_by, random())):
+            if cluster_info['size'] == 1:   
+                cluster_info['name'] = 'Singleton Cluster'
+                cluster_info['info'] = ''
+                cluster_info['number'] = -1
             else:
-                self.clusters_info[cluster_id]['name'] = 'Cluster {}'.format(index_cluster + 1)
-                if self.write_clusters_info:
-                    self.clusters_info[cluster_id]['name'] += ' (dist={:.2f},size={},depth={})'.format(100*cluster_info['dist'],cluster_info['size'],cluster_info['depth'])
+                cluster_info['name'] = 'Cluster {}'.format(index_cluster)
+                cluster_info['info'] = ' (dist={:.2f},size={},depth={})'.format(100*cluster_info['dist'],cluster_info['size'],cluster_info['depth'])
+                cluster_info['number'] = index_cluster
+                index_cluster += 1
 
     def move_images(self):
         if osp.exists(self.clusters_dir):
             rmtree(self.clusters_dir)
         os.makedirs(self.clusters_dir)
-        for (song, cluster_id) in self.clusters.items():
-            cluster_path = osp.join(self.clusters_dir, self.clusters_info[cluster_id]['name'])
-            if not osp.exists(cluster_path):
-                os.makedirs(cluster_path)
-            im = song + '.png'
-            copyfile(osp.join(self.im_dir, im), osp.join(cluster_path, im))
+        for cluster_info in self.clusters.values():
+            if cluster_info['number'] < self.n_folders:
+                if self.write_info:
+                    cluster_path = osp.join(self.clusters_dir, cluster_info['name'] + cluster_info['info'])
+                else:
+                    cluster_path = osp.join(self.clusters_dir, cluster_info['name'])
+                if not osp.exists(cluster_path):
+                    os.makedirs(cluster_path)
+                for song in cluster_info['songs']:
+                    im = song + '.png'
+                    copyfile(osp.join(self.im_dir, im), osp.join(cluster_path, im))
 
     def run(self):
         self.get_clusters()
@@ -83,38 +105,15 @@ class SongClustering(object):
         with open(osp.join(self.res_dir, 'clusters.json'), 'w') as clusters:
             json.dump(self.clusters, clusters, indent=2)
             clusters.close()
-        with open(osp.join(self.res_dir, 'clusters_info.json'), 'w') as clusters_info:
-            json.dump(self.clusters_info, clusters_info, indent=2)
-            clusters_info.close()
 
 
-
-class SongNeighbouring(object):
+class SongNeighbouring(ClusteringParams):
 
     def __init__(self, cmd):
-        self.im_dir = cmd['im_dir']
-        self.res_dir = cmd['res_dir']
+        super().__init__(cmd)
         self.neighbours_dir = cmd['neighbours_dir']
-
-        self.n_neighbours = cmd['n_neighbours']
-        self.write_neighbours_info = cmd['write_neighbours_info']
-        self.sub_sample = cmd['sub_sample']
-        self.random_sub_sample = cmd['random_sub_sample']
-
+        self.n_neighbours = min(cmd['n_neighbours'], self.n_songs - 1)
         self.neighbours = {}
-
-        self.__get_measures__()
-
-    def __get_measures__(self):
-        self.dists = np.loadtxt(osp.join(self.res_dir, 'dists.txt'), delimiter='\t')
-        self.song_dict = {}
-        for line in open(osp.join(self.res_dir, 'song_list.txt'), 'r'):
-            index_song, song = line.split('\n')[0].split('\t')
-            index_song = int(index_song)
-            self.song_dict[index_song] = song.split('.txt')[0]
-        self.n_songs = len(self.song_dict)
-        if self.sub_sample > self.n_songs:
-            self.sub_sample = None
 
     def get_neighbours(self):
         neighbours = {}
@@ -123,39 +122,49 @@ class SongNeighbouring(object):
             neighbours = {}
             neighbours_dists = self.dists[index_song, :]
             neighs = neighbours_dists <= np.percentile(neighbours_dists, (100.*(self.n_neighbours + 1))/self.n_songs)
-            neighbours_dists = np.mean(neighbours_dists[neighs])
+            neighbour_songs = {}
             for index_n in np.where(neighs)[0]:
                 if index_n != index_song:
-                    neighbours[self.song_dict[index_n]] = self.dists[index_song, index_n]
-            self.neighbours[song]['dist'] = neighbours_dists
-            self.neighbours[song]['neighbours'] = neighbours
+                    neighbour_songs[self.song_dict[index_n]] = {
+                          'dist' : self.dists[index_song, index_n],
+                    }
+            for index_song, song_info in enumerate(sorted(neighbour_songs.values(), key=lambda x:x['dist'])):
+                song_info['number'] = index_song + 1
+
+            neighbours_dists = neighbours_dists[neighs]
+            self.neighbours[song] = {
+                  'dist' : np.mean(neighbours_dists),
+                  'std_dist' : np.std(neighbours_dists),
+                  'center' : song,
+                  'neighbours' : neighbour_songs
+            }
+
+        index_song = 1
+        for song_info in sorted(self.neighbours.values(), key=lambda x:x.get(self.order_by, random())):
+            song_info['name'] = 'Song {}: {}'.format(index_song, song_info['center'])
+            song_info['info'] = ' (dist={:.2f})'.format(100*song_info['dist'])
+            song_info['number'] = index_song
+            index_song += 1
 
     def move_images(self):
         if osp.exists(self.neighbours_dir):
             rmtree(self.neighbours_dir)
         os.makedirs(self.neighbours_dir)
-        if self.sub_sample is None:
-            list_neighbours = list(self.neighbours.items())
-        elif self.random_sub_sample:
-            list_neighbours = sample(list(self.neighbours.items()), self.sub_sample)
-        else:
-            list_neighbours = sorted(self.neighbours.items(), key=lambda x:x[1].get('dist', 1))[:self.sub_sample]
-        
-        for (song, neigh_infos) in list_neighbours:
-            neighbours_dists = neigh_infos['dist']
-            neighbours = neigh_infos['neighbours']
-            dir_name = song
-            if self.write_neighbours_info:
-                dir_name += ' (dist={:.2f})'.format(100*neighbours_dists)
-            if not osp.exists(osp.join(self.neighbours_dir, dir_name)):
-                os.makedirs(osp.join(self.neighbours_dir, dir_name))
-
-            im = song + '.png'
-            copyfile(osp.join(self.im_dir, im), osp.join(self.neighbours_dir, dir_name, 'CENTER: ' + im))
-            for (index_neighbour, (neighbour,_)) in enumerate(sorted(neighbours.items(), key=lambda x:x[1])):
-                im = neighbour + '.png'
-                copyfile(osp.join(self.im_dir, im), osp.join(self.neighbours_dir, dir_name, 'N{}: '.format(index_neighbour + 1) + im))
-        
+        for song_info in self.neighbours.values():
+            if song_info['number'] <= self.n_folders:
+                if self.write_info:
+                    song_path = osp.join(self.neighbours_dir, song_info['name'] + song_info['info'])
+                else:
+                    song_path = osp.join(self.neighbours_dir, song_info['name'])
+                if not osp.exists(song_path):
+                    os.makedirs(song_path)
+                im = song_info['center'] + '.png'
+                im_copy = 'Center: ' + im
+                copyfile(osp.join(self.im_dir, im), osp.join(song_path, im_copy))
+                for neigh, neigh_info in song_info['neighbours'].items():
+                    im = neigh + '.png'
+                    im_copy = 'N{}: '.format(neigh_info['number']) + im
+                    copyfile(osp.join(self.im_dir, im), osp.join(song_path, im_copy))
 
     def run(self):
         self.get_neighbours()
@@ -166,25 +175,22 @@ class SongNeighbouring(object):
             neighbours.close()
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--im_dir', type=str, default='images')
+    parser.add_argument('--im_dir', type=str, default='data/images')
     parser.add_argument('--res_dir', type=str, default='results')
+    parser.add_argument('--order_by', type=str, default='dist')
+    parser.add_argument('--n_folders', type=int, default=None)
+    parser.add_argument('--write_info', type=int, default=1)
 
-    parser.add_argument('--clusters_dir', type=str, default='clusters')
+    parser.add_argument('--clusters_dir', type=str, default='results/clusters')
     parser.add_argument('--AC_division', type=int, default=2)
-    parser.add_argument('--clusters_threshold', type=int, default=10)
-    parser.add_argument('--clusters_ordering', type=str, default='size')
-    parser.add_argument('--write_clusters_info', type=int, default=1)
+    parser.add_argument('--cluster_size_threshold', type=int, default=3)
 
-    parser.add_argument('--neighbours_dir', type=str, default='neighbours')
-    parser.add_argument('--n_neighbours', type=int, default=10)
-    parser.add_argument('--write_neighbours_info', type=int, default=1)
-    parser.add_argument('--sub_sample', type=int, default=20)
-    parser.add_argument('--random_sub_sample', type=int, default=1)
+    parser.add_argument('--neighbours_dir', type=str, default='results/neighbours')
+    parser.add_argument('--n_neighbours', type=int, default=3)
     cmd = vars(parser.parse_args())
     sc = SongClustering(cmd)
     sc.run()
-    #sn = SongNeighbouring(cmd)
-    #sn.run()
+    sn = SongNeighbouring(cmd)
+    sn.run()
